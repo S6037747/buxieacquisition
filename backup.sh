0604a762f29a4501ba2b0f8db6bdcfd7dbc3e9d9
S603774 <jeremyzhou2002@gmail.com>
2025-07-17 20:34:58 +0200

    Initial logging database, small automation fixes

--- backup.sh
+++ backup.sh
@@ -3,11 +3,11 @@
 while true; do
   TIMESTAMP=$(date +%F-%H-%M)
 
-  # Cleanup: delete backups older than 12 weeks (84 days)
   find /backups -maxdepth 1 -type d -mtime +84 -exec rm -rf {} \;
 
   mongodump --uri='mongodb://root:root123@mongodb:27017/database?authSource=admin&directConnection=true' --out=/backups/$TIMESTAMP
-  node ../backups/logBackup.js
 
-  sleep 604800  # 7 days
+  docker exec backend node /app/backups/logBackup.js
+
+  sleep 604800
 done
